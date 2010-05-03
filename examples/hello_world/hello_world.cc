2a9f6658a428d8df3c24b685286dc9dc843190c7
dspringer@google.com <dspringer@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2010-05-03 23:18:38 +0000

    Update hello_world based on feedback from a Developer Kitchen.  This version is a little easier to navigate, and hopefully makes changing '42' to some other number easier.
    Review URL: http://codereview.chromium.org/1739024
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@303 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- examples/hello_world/hello_world.cc
+++ examples/hello_world/hello_world.cc
@@ -10,40 +10,40 @@
 #if defined(__native_client__)
 #include <nacl/nacl_npapi.h>
 #else
-// Building a trusted plugin for debugging.
+// Building a develop version for debugging.
 #include "third_party/npapi/bindings/npapi.h"
 #include "third_party/npapi/bindings/nphostapi.h"
 #endif
 
-// Forward declarations of npruntime entry points.
-static NPObject* Allocate(NPP npp, NPClass* npclass);
-static void Deallocate(NPObject* object);
-static void Invalidate(NPObject* object);
-static bool HasMethod(NPObject* obj, NPIdentifier methodName);
-static bool Invoke(NPObject* obj, NPIdentifier methodName,
-                   const NPVariant *args, uint32_t argCount, NPVariant *result);
-static bool InvokeDefault(NPObject *obj, const NPVariant *args,
-                          uint32_t argCount, NPVariant *result);
-static bool HasProperty(NPObject *obj, NPIdentifier propertyName);
-static bool GetProperty(NPObject *obj, NPIdentifier propertyName,
-                        NPVariant *result);
-static bool SetProperty(NPObject* object,
-                        NPIdentifier name,
-                        const NPVariant* value);
+// These are the method names as JavaScript sees them.
+static const char* kHelloWorldMethodId = "helloworld";
+static const char* kFortyTwoMethodId = "fortytwo";
 
-/* Global Variables */
-static NPClass npcRefClass = {
-  NP_CLASS_STRUCT_VERSION,
-  Allocate,
-  Deallocate,
-  Invalidate,
-  HasMethod,
-  Invoke,
-  InvokeDefault,
-  HasProperty,
-  GetProperty,
-  SetProperty
-};
+// This is the module's function that does the work to set the value of the
+// result variable to '42'.  The Invoke() function that called this function
+// then returns the result back to the browser as a JavaScript value.
+static bool FortyTwo(NPVariant *result) {
+  if (result) {
+    INT32_TO_NPVARIANT(42, *result);
+  }
+  return true;
+}
+
+// This function creates a string in the browser's memory pool and then returns
+// a variable containing a pointer to that string.  The variable is later
+// returned back to the browser by the Invoke() function that called this.
+static bool HelloWorld(NPVariant *result) {
+  if (result) {
+    const char *msg = "hello, world.";
+    const int msg_length = strlen(msg) + 1;
+    // Note: |msg_copy| will be freed later on by the browser, so it needs to
+    // be allocated here with NPN_MemAlloc().
+    char *msg_copy = reinterpret_cast<char*>(NPN_MemAlloc(msg_length));
+    strncpy(msg_copy, msg, msg_length);
+    STRINGN_TO_NPVARIANT(msg_copy, msg_length - 1, *result);
+  }
+  return true;
+}
 
 static NPObject* Allocate(NPP npp, NPClass* npclass) {
   return new NPObject;
@@ -53,76 +53,68 @@ static void Deallocate(NPObject* object) {
   delete object;
 }
 
-static void Invalidate(NPObject* object) {
-}
-
-/* Note that this HasMethod() always returns true, and below Invoke    */
-/* always uses InvokeDefault which returns 42. The commented code      */
-/* below shows how you would examine the method name in case you       */
-/* wanted to have multiple methods with different behaviors.           */
-static bool HasMethod(NPObject* obj, NPIdentifier methodName) {
-  return true;
+// Return |true| if |method_name| is a recognized method.
+static bool HasMethod(NPObject* obj, NPIdentifier method_name) {
+  char *name = NPN_UTF8FromIdentifier(method_name);
+  bool is_method = false;
+  if (!strcmp((const char *)name, kHelloWorldMethodId)) {
+    is_method = true;
+  } else if (!strcmp((const char*)name, kFortyTwoMethodId)) {
+    is_method = true;
+  }
+  NPN_MemFree(name);
+  return is_method;
 }
 
 static bool InvokeDefault(NPObject *obj, const NPVariant *args,
                           uint32_t argCount, NPVariant *result) {
   if (result) {
-    INT32_TO_NPVARIANT(42, *result);
+    NULL_TO_NPVARIANT(*result);
   }
   return true;
 }
 
-static bool InvokeHW(NPObject *obj, const NPVariant *args,
-                     uint32_t argCount, NPVariant *result) {
-  if (result) {
-    const char *msg = "hello, world.";
-    const int msglength = strlen(msg) + 1;
-    /* Note: msgcopy will be freed downstream by NPAPI, so */
-    /* it needs to be allocated here with NPN_MemAlloc.    */
-    char *msgcopy = reinterpret_cast<char*>(NPN_MemAlloc(msglength));
-    strncpy(msgcopy, msg, msglength);
-    STRINGN_TO_NPVARIANT(msgcopy, msglength - 1, *result);
-  }
-  return true;
-}
-
-/* Note that this Invoke() always calls InvokeDefault, which returns 42.    */
-/* The commented code below shows how you would examine the method name     */
-/* in case you wanted to have multiple methods with different behaviors.    */
-static bool Invoke(NPObject* obj, NPIdentifier methodName,
-                   const NPVariant *args, uint32_t argCount,
+// Invoke() is called by the browser to invoke a function object whose name
+// is |method_name|.
+static bool Invoke(NPObject* obj,
+                   NPIdentifier method_name,
+                   const NPVariant *args,
+                   uint32_t arg_count,
                    NPVariant *result) {
-  char *name = NPN_UTF8FromIdentifier(methodName);
+  NULL_TO_NPVARIANT(*result);
+  char *name = NPN_UTF8FromIdentifier(method_name);
+  if (name == NULL)
+    return false;
   bool rval = false;
 
-  if (name && !strcmp((const char *)name, "helloworld")) {
-    rval = InvokeHW(obj, args, argCount, result);
-  } else if (name && !strcmp((const char *)name, "loopforever")) {
-    /* for testing purposes only */
-    while (1);
-  } else {
-    rval = InvokeDefault(obj, args, argCount, result);
+  // Map the method name to a function call.  |result| is filled in by the
+  // called function, then gets returned to the browser when Invoke() returns.
+  if (!strcmp((const char *)name, kHelloWorldMethodId)) {
+    rval = HelloWorld(result);
+  } else if (!strcmp((const char*)name, kFortyTwoMethodId)) {
+    rval = FortyTwo(result);
   }
-  /* Since name was allocated above by NPN_UTF8FromIdentifier,      */
-  /* it needs to be freed here.                                     */
+  // Since name was allocated above by NPN_UTF8FromIdentifier,
+  // it needs to be freed here.
   NPN_MemFree(name);
   return rval;
 }
 
-static bool HasProperty(NPObject *obj, NPIdentifier propertyName) {
-  return false;
-}
-
-static bool GetProperty(NPObject *obj, NPIdentifier propertyName,
-                        NPVariant *result) {
-  return false;
-}
-
-static bool SetProperty(NPObject* object, NPIdentifier name,
-                        const NPVariant* value) {
-  return false;
-}
+// The class structure that gets passed back to the browser.  This structure
+// provides funtion pointers that the browser calls.
+static NPClass kHelloWorldClass = {
+  NP_CLASS_STRUCT_VERSION,
+  Allocate,
+  Deallocate,
+  NULL,  // Invalidate is not implemented
+  HasMethod,
+  Invoke,
+  InvokeDefault,
+  NULL,  // HasProperty is not implemented
+  NULL,  // GetProperty is not implemented
+  NULL,  // SetProperty is not implemented
+};
 
 NPClass *GetNPSimpleClass() {
-  return &npcRefClass;
+  return &kHelloWorldClass;
 }
