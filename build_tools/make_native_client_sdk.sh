3e22db15f96d218e8867959478a9947ed4aec70a
khim@google.com <khim@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2010-05-03 18:09:59 +0000

    Replace undecipherable section names nacl-sdk.tgz and naclsdk_win_x86.tgz with human-readable "Examples & documentation" and "Native Client Toolchain" Also move cygwin subpacakges in SectionGroup...
    Review URL: http://codereview.chromium.org/1870002
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@296 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- build_tools/make_native_client_sdk.sh
+++ build_tools/make_native_client_sdk.sh
@@ -127,13 +127,14 @@ mv "`dirname \"$0\"`"/nacl-sdk.tgz packages
 version["native_client_sdk"]="$SDK_VERSION"
 install["native_client_sdk"]="nacl-sdk.tgz"
 sdesc["native_client_sdk"]="\"Native Client SDK - examples and documentation\""
-category["native_client_sdk"]="Devel"
-category_contents["Devel"]="${category_contents[Devel]} native_client_sdk"
+category["native_client_sdk"]="00000000"
+category_contents["00000000"]="${category_contents[00000000]} native_client_sdk"
+requires["native_client_sdk"]="native_client_toolchain"
 version["native_client_toolchain"]="$SDK_VERSION"
 install["native_client_toolchain"]="naclsdk_win_x86.tgz"
 sdesc["native_client_toolchain"]="\"Native Client SDK - toolchain\""
-category["native_client_toolchain"]="Devel"
-category_contents["Devel"]="${category_contents[Devel]} native_client_toolchain"
+category["native_client_toolchain"]="00000000"
+category_contents["00000000"]="${category_contents[00000000]} native_client_toolchain"
 rm setup/*.lst.gz
 download_addon_packages 2
 if ((include_all_packages)) ; then
@@ -265,12 +266,38 @@ END
 if ! patch --no-backup-if-mismatch <<END
 --- make_native_client_sdk.nsi
 +++ make_native_client_sdk.nsi
-@@ -430 +429,0 @@
--  CreateDirectory "\$INSTDIR\\native_client_sdk_${SDK_VERSION//./_}"
-@@ -1269,2 +1266,0 @@
+@@ -182,8 +182,7 @@
+-SectionGroup "00000000" sec_00000000
+-Section "!nacl-sdk.tgz" sec_00000000_native_client_sdk
++Section "!Examples & documentation" sec_00000000_native_client_sdk
+   SectionIn 1 2
+ SectionEnd
+-Section "!naclsdk_win_x86.tgz" sec_00000000_native_client_toolchain
++Section "!Native Client Toolchain" sec_00000000_native_client_toolchain
+   SectionIn 1 2
+ SectionEnd
+-SectionGroupEnd
++SectionGroup "CygWin" sec_00000000
+@@ -427,2 +426,3 @@
+ SectionEnd
++SectionGroupEnd
+ Section "" sec_PKG_native_client_toolchain
+@@ -431,2 +430,0 @@
 -  CreateDirectory "\$INSTDIR\\sdk"
 -  CreateDirectory "\$INSTDIR\\sdk\\nacl-sdk"
-@@ -4052,3 +4049,5 @@
+@@ -1742 +1739,0 @@
+-  CreateDirectory "\$INSTDIR\\native_client_sdk_${SDK_VERSION//./_}"
+@@ -1815 +1811,0 @@
+-  File "/oname=examples\\hello_world\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\hello_world\\make.cmd"
+@@ -1823 +1818,0 @@
+-  File "/oname=examples\\httpd.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\httpd.cmd"
+@@ -1825 +1819,0 @@
+-  File "/oname=examples\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\make.cmd"
+@@ -1828 +1821,0 @@
+-  File "/oname=examples\\pi_generator\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\pi_generator\\make.cmd"
+@@ -1852 +1822,0 @@
+-  File "/oname=examples\\tumbler\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\tumbler\\make.cmd"
+@@ -4054,3 +4046,5 @@
 -  File "/oname=${CYGWIN_PREFIX}bin\\bash.exe" "packages.unpacked\\bash-4.1.5-0.tar.bz2\\usr\\bin\\bash.exe"
 +  File "/oname=${CYGWIN_PREFIX}bin\\bash.exe" "packages.unpacked\\bash-3.2.49-23.tar.bz2\\usr\\bin\\bash.exe"
 +  File "/oname=${CYGWIN_PREFIX}bin\\bash4.exe" "packages.unpacked\\bash-4.1.5-0.tar.bz2\\usr\\bin\\bash.exe"
@@ -278,19 +305,54 @@ if ! patch --no-backup-if-mismatch <<END
 -  File "/oname=${CYGWIN_PREFIX}bin\\sh.exe" "packages.unpacked\\bash-4.1.5-0.tar.bz2\\usr\\bin\\sh.exe"
 +  File "/oname=${CYGWIN_PREFIX}bin\\sh.exe" "packages.unpacked\\bash-3.2.49-23.tar.bz2\\usr\\bin\\sh.exe"
 +  File "/oname=${CYGWIN_PREFIX}bin\\sh4.exe" "packages.unpacked\\bash-4.1.5-0.tar.bz2\\usr\\bin\\sh.exe"
-@@ -4301,4 +4298,4 @@
+@@ -4303,4 +4299,4 @@
 +  MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}bin\\awk.exe" "\$INSTDIR\\${CYGWIN_PREFIX}bin\\gawk.exe"
    MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}bin\\gawk-3.1.7.exe" "\$INSTDIR\\${CYGWIN_PREFIX}bin\\gawk.exe"
    MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}bin\\pgawk-3.1.7.exe" "\$INSTDIR\\${CYGWIN_PREFIX}bin\\pgawk.exe"
    MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}usr\\share\\man\\man1\\gawk.1" "\$INSTDIR\\${CYGWIN_PREFIX}usr\\share\\man\\man1\\pgawk.1"
 -  MkLink::SoftF "\$INSTDIR\\${CYGWIN_PREFIX}bin\\awk.exe" "gawk.exe"
-@@ -8990,3 +8987,3 @@
+@@ -7046,6 +7040,10 @@
+ Section "" sec_PKG_make
+   SectionIn 1 2
+   SetOutPath \$INSTDIR
++  CreateDirectory "\$INSTDIR\\examples"
++  CreateDirectory "\$INSTDIR\\examples\\hello_world"
++  CreateDirectory "\$INSTDIR\\examples\\pi_generator"
++  CreateDirectory "\$INSTDIR\\examples\\tumbler"
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}usr"
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}bin"
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}usr\\share"
+@@ -7101,2 +7099,6 @@
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}usr\\share\\man\\man1"
++  File "/oname=examples\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\make.cmd"
++  File "/oname=examples\\hello_world\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\hello_world\\make.cmd"
++  File "/oname=examples\\pi_generator\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\pi_generator\\make.cmd"
++  File "/oname=examples\\tumbler\\make.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\tumbler\\make.cmd"
+   File "/oname=${CYGWIN_PREFIX}bin\\make.exe" "packages.unpacked\\make-3.81-2.tar.bz2\\usr\\bin\\make.exe"
+@@ -7433,6 +7435,7 @@
+ Section "" sec_PKG_python
+   SectionIn 1 2
+   SetOutPath \$INSTDIR
++  CreateDirectory "\$INSTDIR\\examples"
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}usr"
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}bin"
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}usr\\include"
+@@ -7471,2 +7474,3 @@
+   CreateDirectory "\$INSTDIR\\${CYGWIN_PREFIX}usr\\share\\man\\man1"
++  File "/oname=examples\\httpd.cmd" "packages.unpacked\\nacl-sdk.tgz\\native_client_sdk_${SDK_VERSION//./_}\\examples\\httpd.cmd"
+   File "/oname=${CYGWIN_PREFIX}bin\\libpython2.5.dll" "packages.unpacked\\python-2.5.5-1.tar.bz2\\usr\\bin\\libpython2.5.dll"
+@@ -8992,3 +8996,3 @@
 -  MkLink::SoftF "\$INSTDIR\\${CYGWIN_PREFIX}bin\\python.exe" "python2.5.exe"
 -  MkLink::SoftF "\$INSTDIR\\${CYGWIN_PREFIX}bin\\python-config" "python2.5-config"
 -  MkLink::SoftF "\$INSTDIR\\${CYGWIN_PREFIX}lib\\libpython2.5.dll.a" "python2.5\\config\\libpython2.5.dll.a"
 +  MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}bin\\python.exe" "\$INSTDIR\\${CYGWIN_PREFIX}bin\\python2.5.exe"
 +  MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}bin\\python-config" "\$INSTDIR\\${CYGWIN_PREFIX}bin\\python2.5-config"
 +  MkLink::Hard "\$INSTDIR\\${CYGWIN_PREFIX}lib\\python2.5.dll.a" "\$INSTDIR\\${CYGWIN_PREFIX}lib\\python2.5\\config\\libpython2.5.dll.a"
+@@ -9032,3 +9035,3 @@
+-  !insertmacro MUI_DESCRIPTION_TEXT \${sec_00000000} ""
+   !insertmacro MUI_DESCRIPTION_TEXT \${sec_00000000_native_client_sdk} "Native Client SDK - examples and documentation"
+   !insertmacro MUI_DESCRIPTION_TEXT \${sec_00000000_native_client_toolchain} "Native Client SDK - toolchain"
++  !insertmacro MUI_DESCRIPTION_TEXT \${sec_00000000} "Small POSIX-like environment. Disable if you already have POSIX-like environment installed and know how to use it."
 END
   then
     exit 1
