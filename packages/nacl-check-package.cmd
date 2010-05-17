97d5073d964819b7408925e04a266c92e4bc8fc5
khim@google.com <khim@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2010-05-17 00:47:14 +0000

    Some autoconf stripts are checking what ld is used by cross-gcc and what as is used by cross-gcc.  Our cygwin tools report that they use /nacl64/bin/as and /nacl64/bin/ld - and this is true because our gcc lives in "toolchain jail", but... cygwin can not find these files!
    Add symlinks to make the tools usable...
    
    Review URL: http://codereview.chromium.org/2115003
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@365 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- packages/nacl-check-package.cmd
+++ packages/nacl-check-package.cmd
@@ -4,4 +4,7 @@ setlocal
 
 PATH=%~dp0..\third_party\cygwin\bin;%PATH%
 
+ln -sfn %~dp0..\toolchain\win_x86\nacl /nacl 2>nul
+ln -sfn %~dp0..\toolchain\win_x86\nacl64 /nacl64 2>nul
+
 bash nacl-check-package.sh %*
