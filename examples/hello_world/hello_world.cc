8e0ec0d1f7ba531471e109ce82d2e3df6d520a53
mball@google.com <mball@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2010-11-17 15:01:34 +0000

    Updated hello_world to use doxygen-style comments Review URL: http://codereview.chromium.org/4834001
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@615 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- examples/hello_world/hello_world.cc
+++ examples/hello_world/hello_world.cc
@@ -2,20 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can
 // be found in the LICENSE file.
 
-// This example demonstrates loading, running and scripting a very simple NaCl
-// module.  To load the NaCl module, the browser first looks for the
-// CreateModule() factory method (ad the end of this file).  It calls
-// CreateModule() once to load the module code from your .nexe.  After the
-// .nexe code is loaded, CreateModule() is not called again.
-//
-// Once the .nexe code is loaded, the browser than calls the CreateInstance()
-// method on the object returned by CreateModule().  It calls CreateInstance()
-// each time it encounters an <embed> tag that references your NaCl module.
-//
-// When the browser encounters JavaScript that references your NaCl module, it
-// calls the GetInstanceObject() method on the object returned from
-// CreateInstance().  In this example, the returned object is a subclass of
-// ScriptableObject, which handles the scripting support.
+/// @file
+/// This example demonstrates loading, running and scripting a very simple NaCl
+/// module.  To load the NaCl module, the browser first looks for the
+/// CreateModule() factory method (at the end of this file).  It calls
+/// CreateModule() once to load the module code from your .nexe.  After the
+/// .nexe code is loaded, CreateModule() is not called again.
+///
+/// Once the .nexe code is loaded, the browser then calls the
+/// HelloWorldModule::CreateInstance()
+/// method on the object returned by CreateModule().  It calls CreateInstance()
+/// each time it encounters an <embed> tag that references your NaCl module.
+///
+/// When the browser encounters JavaScript that references your NaCl module, it
+/// calls the GetInstanceObject() method on the object returned from
+/// CreateInstance().  In this example, the returned object is a subclass of
+/// ScriptableObject, which handles the scripting support.
 
 #include <ppapi/cpp/instance.h>
 #include <ppapi/cpp/module.h>
@@ -25,24 +27,27 @@
 #include <string>
 #include <algorithm>  // for reverse
 
-// These are the method names as JavaScript sees them.
 namespace {
+/// method name for ReverseText, as seen by JavaScript code.
 const char* const kReverseTextMethodId = "reverseText";
+
+/// method name for FortyTwo, as seen by Javascript code.
 const char* const kFortyTwoMethodId = "fortyTwo";
 
-// This is the module's function that does the work to compute the value 42.
-// The ScriptableObject that called this function then returns the result back
-// to the browser as a JavaScript value.
+/// This is the module's function that does the work to compute the value 42.
+/// The ScriptableObject that called this function then returns the result back
+/// to the browser as a JavaScript value.
 int32_t FortyTwo() {
   return 42;
 }
 
-// This function is passed the arg list from the Javascript call to reverseText.
-// It makes sure that there is one argument and that it is a string, returning
-// an error message if it is not.
-// On good input, it reverses the string and returns a message with the
-// original string and the reversed string.  The ScriptableObject that called
-// this function returns this string back to the browser as a Javascript value.
+/// This function is passed the arg list from the JavaScript call to
+/// @a reverseText.
+/// It makes sure that there is one argument and that it is a string, returning
+/// an error message if it is not.
+/// On good input, it reverses the string and returns a message with the
+/// original string and the reversed string.  The ScriptableObject that called
+/// this function returns this string back to the browser as a JavaScript value.
 std::string ReverseText(const std::vector<pp::Var>& args) {
   // There should be exactly one arg, which should be an object
   if (args.size() != 1) {
@@ -63,28 +68,35 @@ std::string ReverseText(const std::vector<pp::Var>& args) {
 }
 }  // namespace
 
-// This class exposes the scripting interface for this NaCl module.  The
-// HasMethod method is called by the browser when executing a method call on
-// the |helloWorld| object (see, e.g. the reverseText() function in
-// hello_world.html).  The name of the JavaScript function (e.g. "fortyTwo") is
-// passed in the |method| paramter as a string pp::Var.  If HasMethod()
-// returns |true|, then the browser will call the Call() method to actually
-// invoke the method.
+/// This class exposes the scripting interface for this NaCl module.  The
+/// HasMethod() method is called by the browser when executing a method call on
+/// the @a helloWorldModule object (see the reverseText() function in
+/// hello_world.html).  The name of the JavaScript function (e.g. "fortyTwo") is
+/// passed in the @a method parameter as a string pp::Var.  If HasMethod()
+/// returns @a true, then the browser will call the Call() method to actually
+/// invoke the method.
 class HelloWorldScriptableObject : public pp::deprecated::ScriptableObject {
  public:
-  // Return |true| if |method| is one of the exposed method names.
+  /// Determines whether a given method is implemented in this object.
+  /// @param method [in] A JavaScript string containing the method name to check
+  /// @param exception Unused
+  /// @return @a true if @a method is one of the exposed method names.
   virtual bool HasMethod(const pp::Var& method, pp::Var* exception);
 
-  // Invoke the function associated with |method|.  The argument list passed in
-  // via JavaScript is marshaled into a vector of pp::Vars.  None of the
-  // functions in this example take arguments, so this vector is always empty.
+  /// Invoke the function associated with @a method.  The argument list passed
+  /// via JavaScript is marshaled into a vector of pp::Vars.  None of the
+  /// functions in this example take arguments, so this vector is always empty.
+  /// @param method [in] A JavaScript string with the name of the method to call
+  /// @param args [in] A list of the JavaScript parameters passed to this method
+  /// @param exception unused
+  /// @return the return value of the invoked method
   virtual pp::Var Call(const pp::Var& method,
                        const std::vector<pp::Var>& args,
                        pp::Var* exception);
 };
 
 bool HelloWorldScriptableObject::HasMethod(const pp::Var& method,
-                                           pp::Var* exception) {
+                                           pp::Var* /* exception */) {
   if (!method.is_string()) {
     return false;
   }
@@ -96,7 +108,7 @@ bool HelloWorldScriptableObject::HasMethod(const pp::Var& method,
 
 pp::Var HelloWorldScriptableObject::Call(const pp::Var& method,
                                          const std::vector<pp::Var>& args,
-                                         pp::Var* exception) {
+                                         pp::Var* /* exception */) {
   if (!method.is_string()) {
     return pp::Var();
   }
@@ -111,49 +123,60 @@ pp::Var HelloWorldScriptableObject::Call(const pp::Var& method,
   return pp::Var();
 }
 
-// The Instance class.  One of these exists for each instance of your NaCl
-// module on the web page.  The browser will ask the Module object to create
-// a new Instance for each occurence of the <embed> tag that has these
-// attributes:
-//     type="application/x-ppapi-nacl-srpc"
-//     nexes="ARM: hello_world_arm.nexe
-//            ..."
-// The Instance can return a ScriptableObject representing itself.  When the
-// browser encounters JavaScript that wants to access the Instance, it calls
-// the GetInstanceObject() method.  All the scripting work is done though
-// the returned ScriptableObject.
+/// The Instance class.  One of these exists for each instance of your NaCl
+/// module on the web page.  The browser will ask the Module object to create
+/// a new Instance for each occurrence of the <embed> tag that has these
+/// attributes:
+/// <pre>
+///     type="application/x-ppapi-nacl-srpc"
+///     nexes="ARM: hello_world_arm.nexe
+///            x86-32: hello_world_x86_32.nexe
+///            x86-64: hello_world_x86_64.nexe"
+/// </pre>
+/// The Instance can return a ScriptableObject representing itself.  When the
+/// browser encounters JavaScript that wants to access the Instance, it calls
+/// the GetInstanceObject() method.  All the scripting work is done through
+/// the returned ScriptableObject.
 class HelloWorldInstance : public pp::Instance {
  public:
   explicit HelloWorldInstance(PP_Instance instance) : pp::Instance(instance) {}
   virtual ~HelloWorldInstance() {}
 
-  // The pp::Var takes over ownership of the HelloWorldScriptableObject.
+  /// @return a new pp::deprecated::ScriptableObject as a JavaScript @a Var
+  /// @note The pp::Var takes over ownership of the HelloWorldScriptableObject
+  ///       and is responsible for deallocating memory.
   virtual pp::Var GetInstanceObject() {
     HelloWorldScriptableObject* hw_object = new HelloWorldScriptableObject();
     return pp::Var(hw_object);
   }
 };
 
-// The Module class.  The browser calls the CreateInstance() method to create
-// an instance of you NaCl module on the web page.  The browser creates a new
-// instance for each <embed> tag with type="application/x-ppapi-nacl-srpc".
+/// The Module class.  The browser calls the CreateInstance() method to create
+/// an instance of you NaCl module on the web page.  The browser creates a new
+/// instance for each <embed> tag with
+/// <code>type="application/x-ppapi-nacl-srpc"</code>.
 class HelloWorldModule : public pp::Module {
  public:
   HelloWorldModule() : pp::Module() {}
   virtual ~HelloWorldModule() {}
 
-  // Create and return a HelloWorldInstance object.
+  /// Create and return a HelloWorldInstance object.
+  /// @param instance [in] a handle to a plug-in instance.
+  /// @return a newly created HelloWorldInstance.
+  /// @note The browser is responsible for calling @a delete when done.
   virtual pp::Instance* CreateInstance(PP_Instance instance) {
     return new HelloWorldInstance(instance);
   }
 };
 
-// Factory function called by the browser when the module is first loaded.
-// The browser keeps a singleton of this module.  It calls the
-// CreateInstance() method on the object you return to make instances.  There
-// is one instance per <embed> tag on the page.  This is the main binding
-// point for your NaCl module with the browser.
 namespace pp {
+/// Factory function called by the browser when the module is first loaded.
+/// The browser keeps a singleton of this module.  It calls the
+/// CreateInstance() method on the object you return to make instances.  There
+/// is one instance per <embed> tag on the page.  This is the main binding
+/// point for your NaCl module with the browser.
+/// @return new HelloWorldModule.
+/// @note The browser is responsible for deleting returned @a Module.
 Module* CreateModule() {
   return new HelloWorldModule();
 }
