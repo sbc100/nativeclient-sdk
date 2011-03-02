01d1b98b159436110c7c7da8e1ab7bb2b1d645a7
dspringer@google.com <dspringer@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-03-02 14:44:14 +0000

    Add #!/usr/bin/python to generate_nmf.py.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1448
    TEST=Manual.  d/l and extract the SDK and execute:
      cd project_templates
      ./generate_nmf.py --help
    
    Review URL: http://codereview.chromium.org/6596098
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@747 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- project_templates/generate_nmf.py
+++ project_templates/generate_nmf.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+#
+# Copyright 2011, The Native Client SDK Authors.  All Rights Reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
+
 import sys
 import optparse
 
