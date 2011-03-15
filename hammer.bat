47a54f92691b8ee6efc9565dda3756cee0b199e1
mball@google.com <mball@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-03-15 18:48:52 +0000

    SDK: Add a bot mode to the main scons file
    This change includes substantial infrastructure cleanup for the SDK.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1440
    TEST= Manually ran hammer.sh to see whether it works as expected.
    
    Review URL: http://codereview.chromium.org/6531013
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@771 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- hammer.bat
+++ hammer.bat
@@ -2,8 +2,10 @@
 @rem Use of this source code is governed by a BSD-style license that can be
 @rem found in the LICENSE file.
 
+@echo off
 setlocal
 
 set SCONS_DIR=%~dp0third_party\scons\scons-local
 set PYMOX=%~dp0third_party\pymox"
+set NACL_SDK_ROOT=%~dp0
 @call %~dp0third_party\swtoolkit\hammer.bat %*
