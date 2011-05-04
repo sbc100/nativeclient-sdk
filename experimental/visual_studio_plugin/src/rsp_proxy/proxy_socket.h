2ad30c2bc4ec46cda6555c44f3bb667de76cd75c
mlinck@google.com <mlinck@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-05-04 16:45:30 +0000

    Adds a simple proxy that forwards port 4014
    The port is forwarded from the loopback interface to the global interface.
    This is to be used for debugging purposes only.
    
    BUG=https://tracker.corp.google.com/story/show/366489
    TEST=manual/experimental
    Review URL: http://codereview.chromium.org/6920002
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@839 050acbb0-2703-11df-ab0a-9f3f633ae91d

---
+++ experimental/visual_studio_plugin/src/rsp_proxy/proxy_socket.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXPERIMENTAL_VISUAL_STUDIO_PLUGIN_SRC_RSP_PROXY_PROXY_SOCKET_H_
+#define EXPERIMENTAL_VISUAL_STUDIO_PLUGIN_SRC_RSP_PROXY_PROXY_SOCKET_H_
+
+#include <winsock.h>
+#include <string>
+
+namespace debug {
+
+// Implements a listening socket.
+// Example:
+// ListeningSocket lsn;
+// lsn.Setup(4014);
+// Socket conn;
+// if (lsn.Accept(&conn, 200))
+//   DoSomethingWithNewConnection(conn);
+
+class Socket;
+class ListeningSocket {
+ public:
+  ListeningSocket();
+  ~ListeningSocket();
+
+  bool Setup(int port);  // Listens on port.
+  bool Accept(Socket* new_connection, int wait_ms);
+  void Close();
+
+ private:
+  SOCKET sock_;
+  bool init_success_;
+};
+
+// Implements a raw socket interface.
+// Example:
+// Socket conn;
+// if (conn.ConnectTo("172.29.20.175", 4016))
+//   DoSomethingWithNewConnection(conn);
+
+class Socket {
+ public:
+  Socket();
+  ~Socket();
+
+  bool ConnectTo(const std::string& host, int port);
+  bool IsConnected() const;
+  void Close();
+  size_t Read(void* buff, size_t sz, int wait_ms);
+  size_t Write(const void* buff, size_t sz, int wait_ms);
+  void WriteAll(const void* buff, size_t sz);
+
+ private:
+  void AttachTo(SOCKET sock);
+
+  SOCKET sock_;
+  bool init_success_;
+  friend class ListeningSocket;
+};
+}  // namespace debug
+#endif  // EXPERIMENTAL_VISUAL_STUDIO_PLUGIN_SRC_RSP_PROXY_PROXY_SOCKET_H_
+
+
---
+++ experimental/visual_studio_plugin/src/rsp_proxy/rsp_proxy.cc
@@ -0,0 +1,95 @@
+// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This application forwards the port that the rsp debug stub opens on the
+// loopback interface, and forwards it to the public interface, so that a
+// remote debugger can connect to it.
+
+#include <conio.h>
+
+#include "rsp_proxy/proxy_socket.h"
+
+namespace {
+enum ConnectionState {CONNECTING, CONNECTED};
+const int kBufferSize = 512;
+const int kListenMs = 20;
+const int kReadWaitMs = 20;
+}
+
+void Disconnect(debug::ListeningSocket client_listening_socket,
+                debug::Socket client_socket,
+                debug::Socket server_socket) {
+  client_listening_socket.Close();
+  client_socket.Close();
+  server_socket.Close();
+}
+
+int main(int argc, char **argv) {
+  debug::Socket server_side_socket;
+  debug::ListeningSocket client_side_listening_socket;
+  debug::Socket client_side_socket;
+  ConnectionState state = CONNECTING;
+
+  client_side_listening_socket.Setup(4014);
+
+  while (true) {
+    if (CONNECTING == state) {
+      if (!server_side_socket.IsConnected()) {
+        server_side_socket.ConnectTo("127.0.0.1", 4014);
+        if (server_side_socket.IsConnected())
+          printf("Connected to the debug_stub.\n");
+      } else if (!client_side_socket.IsConnected()) {
+        client_side_listening_socket.Accept(&client_side_socket, kListenMs);
+        if (client_side_socket.IsConnected())
+          printf("Got connection from host\n");
+      } else {
+        state = CONNECTED;
+      }
+      continue;
+    }
+
+    if (CONNECTED == state) {
+      if (!server_side_socket.IsConnected() ||
+          !client_side_socket.IsConnected()) {
+        Disconnect(client_side_listening_socket,
+                   client_side_socket,
+                   server_side_socket);
+        state = CONNECTING;
+        continue;
+      }
+    }
+
+    char buff[kBufferSize];
+    size_t read_bytes = client_side_socket.Read(buff,
+                                                sizeof(buff) - 1,
+                                                kReadWaitMs);
+    if (read_bytes > 0) {
+      buff[read_bytes] = 0;
+      printf("h>%s\n", buff);
+      server_side_socket.WriteAll(buff, read_bytes);
+    }
+
+    read_bytes = server_side_socket.Read(buff, sizeof(buff) - 1, kReadWaitMs);
+    if (read_bytes > 0) {
+      buff[read_bytes] = 0;
+      printf("t>%s\n", buff);
+      client_side_socket.WriteAll(buff, read_bytes);
+    }
+
+    if (_kbhit()) {
+      char c = getchar();
+      if ('q' == c) {
+        break;
+      }
+    }
+  }
+
+  if (CONNECTED == state) {
+    Disconnect(client_side_listening_socket,
+               client_side_socket,
+               server_side_socket);
+  }
+
+  return 0;
+}
