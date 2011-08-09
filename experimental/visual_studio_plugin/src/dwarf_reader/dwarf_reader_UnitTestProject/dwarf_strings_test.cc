fd79fd370958bf4ec434dadfa424c4b34f4783a2
mlinck@google.com <mlinck@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-08-09 17:12:02 +0000

    Adding dwarf_reader tests
    BUG=Internal
    TEST=bots
    
    Review URL: http://codereview.chromium.org/7598018
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1023 050acbb0-2703-11df-ab0a-9f3f633ae91d

---
+++ experimental/visual_studio_plugin/src/dwarf_reader/dwarf_reader_UnitTestProject/dwarf_strings_test.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "dwarf_reader/dwarf_strings.h"
+#include "gtest/gtest.h"
+
+namespace {
+// Unit tests start here.
+// TODO(mlinck) replace this trival sample test with tests for new code as it
+// is being written.
+TEST(DwarfStringsTest, DwarfAttributeName) {
+  const char * name = dwarf_reader::DwarfAttributeName(
+      dwarf2reader::DW_AT_location);
+  EXPECT_STREQ(name, "location");
+}
+}  // namespace
+
