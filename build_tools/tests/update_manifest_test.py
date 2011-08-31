2d61aae787f624df76d4525aad2cd935778ec385
gwink@google.com <gwink@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-08-31 19:15:38 +0000

    Preliminary work on ptyhon scrip update_manifest. Implements cmd-line options and basic file I/O. Review URL: http://codereview.chromium.org/7800002
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1091 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- build_tools/tests/update_manifest_test.py
+++ build_tools/tests/update_manifest_test.py
@@ -12,28 +12,80 @@ import mox
 import os
 import subprocess
 import sys
+import tempfile
 import unittest
 
 from build_tools import update_manifest
 
 
-class TestUpdateManifest(unittest.TestCase):
-  """This class tests basic functionality of the update_manifest package"""
-  def setUp(self):
-    pass
+class FakeOptions(object):
+  ''' Just a placeholder for options '''
+  pass
 
-  def testJSONBoilerplate(self):
-    json_boilerplate=(
+
+class TestUpdateManifest(unittest.TestCase):
+  ''' Test basic functionality of the update_manifest package '''
+  def setUp(self):
+    self._json_boilerplate=(
         '{\n'
         '  "bundles": [],\n'
         '  "manifest_version": 1\n'
         '}\n')
-    manifest = update_manifest.SDKManifest()
-    self.assertEqual(manifest.GetManifestString(), json_boilerplate)
+    self._temp_dir = tempfile.gettempdir()
+    # os.path.join('build_tools', 'tests', 'test_archive')
+    self._manifest = update_manifest.SDKManifest()
+
+  def testJSONBoilerplate(self):
+    ''' Test creating a manifest object'''
+    self.assertEqual(self._manifest.GetManifestString(),
+                     self._json_boilerplate)
     # Test using a manifest file with a version that is too high
     self.assertRaises(Exception,
-                      manifest.LoadManifestString,
+                      self._manifest.LoadManifestString,
                       '{"manifest_version": 2}')
+
+  def testWriteLoadManifestFile(self):
+    ''' Test writing to and loading from a manifest file'''
+    # Remove old test file
+    file_path = os.path.join(self._temp_dir, 'temp_manifest.json')
+    if os.path.exists(file_path):
+      os.remove(file_path)
+    # Create a basic manifest file
+    manifest_file = update_manifest.SDKManifestFile(file_path)
+    manifest_file._WriteFile();
+    self.assertTrue(os.path.exists(file_path))
+    # Test re-loading the file
+    manifest_file._manifest._manifest_data['manifest_version'] = 0
+    manifest_file._LoadFile()
+    self.assertEqual(manifest_file._manifest.GetManifestString(),
+                     self._json_boilerplate)
+
+  def testValidateBundleName(self):
+    ''' Test validating good and bad bundle names '''
+    self.assertTrue(
+        self._manifest._ValidateBundleName('A_Valid.Bundle-Name(1)'))
+    self.assertFalse(self._manifest._ValidateBundleName('A bad name'))
+    self.assertFalse(self._manifest._ValidateBundleName('A bad/name'))
+    self.assertFalse(self._manifest._ValidateBundleName('A bad;name'))
+    self.assertFalse(self._manifest._ValidateBundleName('A bad,name'))
+
+  def testUpdateManifestVersion(self):
+    ''' Test updating the manifest version number '''
+    options = FakeOptions()
+    options.manifest_version = 99
+    self.assertEqual(self._manifest._manifest_data['manifest_version'], 1)
+    self._manifest._UpdateManifestVersion(options)
+    self.assertEqual(self._manifest._manifest_data['manifest_version'], 99)
+
+  def testVerifyAllOptionsConsumed(self):
+    ''' Test function _VerifyAllOptionsConsumed '''
+    options = FakeOptions()
+    options.opt1 = None
+    self.assertTrue(self._manifest._VerifyAllOptionsConsumed(options))
+    options.opt2 = 'blah'
+    self.assertRaises(Exception,
+                      self._manifest._VerifyAllOptionsConsumed,
+                      options)
 
 def main():
   suite = unittest.TestLoader().loadTestsFromTestCase(TestUpdateManifest)
--- build_tools/update_manifest.py
+++ build_tools/update_manifest.py
@@ -7,6 +7,24 @@
 
 import json
 import optparse
+import os
+import shutil
+import string
+import sys
+import tempfile
+
+class ManifestException(Exception):
+  ''' Exceptions raised within update_manifest '''
+  pass
+
+
+class Bundle(dict):
+  ''' A placeholder for sdk bundle information. We derive Bundle from
+      dict so that it is easily serializable. '''
+  def __init__(self, name):
+    ''' Create a new bundle with the given bundle name. '''
+    self['name'] = name
+
 
 class SDKManifest(object):
   '''This class contains utilities for manipulation an SDK manifest string
@@ -17,7 +35,7 @@ class SDKManifest(object):
   def __init__(self):
     '''Create a new SDKManifest object with default contents'''
     self.MANIFEST_VERSION = 1
-    self._manifest = {
+    self._manifest_data = {
         "manifest_version": self.MANIFEST_VERSION,
         "bundles": []
     }
@@ -25,44 +43,249 @@ class SDKManifest(object):
 
   def _ValidateManifest(self):
     '''Validate the Manifest file and raises an exception for problems'''
-    if self._manifest["manifest_version"] > self.MANIFEST_VERSION:
-      raise Exception("Manifest version too large: %s" %
-                      self._manifest["manifest_version"])
+    if self._manifest_data["manifest_version"] > self.MANIFEST_VERSION:
+      raise ManifestException("Manifest version too high: %s" %
+                              self._manifest_data["manifest_version"])
 
     # TODO(mball,gwink) check other aspects of manifest file here
 
+  def _ValidateBundleName(self, name):
+    ''' Verify that name is a valid bundle.
+
+    Args:
+      name: the proposed name for the bundle.
+
+    Return:
+      True if the name is valid for a bundle, False otherwise.'''
+    valid_char_set = '()-_.%s%s' % (string.ascii_letters, string.digits)
+    name_len = len(name)
+    return (name_len > 0 and all(c in valid_char_set for c in name))
+
+  def _GetBundle(self, name):
+    ''' Get a bundle from the array of bundles.
+
+    Args:
+      name: the name of the bundle to return.
+    Return:
+      The bundle with the given name, or None if it is not found.'''
+    if not 'bundle' in self._manifest_data:
+      return None
+    bundles = self._manifest_data['bundle']
+    for bundle in bundles:
+      if 'name' in bundle and bundle['name'] == name:
+        return bundle
+    return None
+
+  def _AddBundle(self, bundle):
+    ''' Add a bundle to the manifest
+
+    Args:
+      bundle: to the bundle to add to the manifest.'''
+    if not 'bundle' in self._manifest_data:
+      self._manifest_data['bundles'] = []
+    self._manifest_data['bundles'].append(bundle)
+
+  def _UpdateManifestVersion(self, options):
+    ''' Update the manifest version number from the options
+
+    Args:
+      options: options data containing an attribute self.manifest_version '''
+    version_num = int(options.manifest_version)
+    self._manifest_data['manifest_version'] = version_num
+    del options.manifest_version
+
+  def _UpdateBundle(self, options):
+    ''' Update or setup a bundle from the options.
+
+    Args:
+      options: options data containing at least a valid bundle_name
+               attribute. Other relevant bundle attributes will also be
+               used (and consumed) by this function. '''
+    # Get and validate the bundle name
+    if not self._ValidateBundleName(options.bundle_name):
+      raise ManifestException('Invalid bundle name: "%s"' %
+                              options.bundle_name)
+    bundle_name = options.bundle_name
+    del options.bundle_name
+    # Get the corresponding bundle, or create it.
+    bundle = self._GetBundle(bundle_name)
+    if not bundle:
+      bundle = Bundle(bundle_name)
+      self._AddBundle(bundle)
+    # TODO(gwink) create/modify bundle per options.
+
+  def _VerifyAllOptionsConsumed(self, options):
+    ''' Verify that all the options have been used. Raise an exception if
+        any valid option has not been used. Returns True if all options have
+        been consumed.
+
+    Args:
+      options: the object containg the remaining unused options attributes.'''
+    # Any option left in the list should have value = None
+    for key, val in options.__dict__.items():
+      if val != None:
+        raise ManifestException("Unused option: %s" % key)
+    return True;
+
 
   def LoadManifestString(self, json_string):
-    '''Load a JSON manifest string
+    ''' Load a JSON manifest string. Raises an exception if json_string
+        is not well-formed JSON.
 
     Args:
       json_string: a JSON-formatted string containing the previous manifest'''
     new_manifest = json.loads(json_string)
     for key, value in new_manifest.items():
-      self._manifest[key] = value
+      self._manifest_data[key] = value
     self._ValidateManifest()
 
 
   def GetManifestString(self):
     '''Returns the current JSON manifest object, pretty-printed'''
-    pretty_string = json.dumps(self._manifest, sort_keys=False, indent=2)
+    pretty_string = json.dumps(self._manifest_data, sort_keys=False, indent=2)
     # json.dumps sometimes returns trailing whitespace and does not put
     # a newline at the end.  This code fixes these problems.
     pretty_lines = pretty_string.split('\n')
     return '\n'.join([line.rstrip() for line in pretty_lines]) + '\n'
 
+  def UpdateManifest(self, options):
+    ''' Update the manifest object with value from the command-line options
 
-def main():
+    Args:
+      options: options object containing attribute for the command-line options.
+               Note that all the non-trivial options are consumed.
+    '''
+    # Go over all the options and update the manifest data accordingly.
+    # Valid options are consumed as they are used. This gives us a way to
+    # verify that all the options are used.
+    if options.manifest_version != None:
+      self._UpdateManifestVersion(options)
+    if options.bundle_name != None:
+      self._UpdateBundle(options)
+    self._VerifyAllOptionsConsumed(options)
+    self._ValidateManifest()
+
+
+class SDKManifestFile(object):
+  ''' This class provides basic file I/O support for manifest objects.'''
+
+  def __init__(self, json_filepath):
+    '''Create a new SDKManifest object with default contents.
+
+    Args:
+      json_filepath: path to jason file to read/write, or None to write a new
+      manifest file to stdout.'''
+    self._json_filepath = json_filepath
+    self._manifest = SDKManifest()
+
+  def _LoadFile(self):
+    '''Load the manifest from the JSON file. This function returns quietly
+       if the file doesn't exit.'''
+    if not os.path.exists(self._json_filepath):
+      return
+
+    with open(self._json_filepath, 'r') as f:
+      json_string = f.read()
+    if json_string:
+      self._manifest.LoadManifestString(json_string)
+
+
+  def _WriteFile(self):
+    '''Write the json data to the file. If not file name was specified, the
+       data is written to stdout.'''
+    json_string = self._manifest.GetManifestString()
+    if not self._json_filepath:
+      # No file is specified; print the json data to stdout
+      sys.stdout.write(json_string)
+    else:
+      # Write the JSON data to a temp file.
+      temp_file_name = None
+      with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+        f.write(json_string)
+        temp_file_name = f.name
+      # Move the temp file to the actual file.
+      if os.path.exists(self._json_filepath):
+        os.remove(self._json_filepath)
+      shutil.move(temp_file_name, self._json_filepath)
+
+  def UpdateWithOptions(self, options):
+    if self._json_filepath:
+      self._LoadFile()
+    self._manifest.UpdateManifest(options)
+    self._WriteFile()
+
+
+def main(argv):
   '''Main entry for update_manifest.py'''
 
   parser = optparse.OptionParser(
       usage="Usage: %prog [options] [manifest_file]")
 
-  # TODO(mball,gwink) Add command-line options to allow specifying the
-  # URLs of the SDK bundles.
+  # Setup options
+  parser.add_option(
+      '-A', '--all_tgz', dest='all_tgz_url',
+      default=None,
+      help='URL for an all-platform tgz archive.')
+  parser.add_option(
+      '-B', '--bundle_revision', dest='bundle_revision',
+      default=None,
+      help='Required: Revision number for the bundle.')
+  parser.add_option(
+      '-b', '--bundle_version', dest='bundle_version',
+      default=None,
+      help='Optional: Version number for the bundle.')
+  parser.add_option(
+      '-d', '--description', dest='desc',
+      default=None,
+      help='Required: Description for this bundle.')
+  parser.add_option(
+      '-M', '--mac_tgz', dest='mac_tgz_url',
+      default=None,
+      help='URL for the Mac tgz archive.')
+  parser.add_option(
+      '-L', '--linux_tgz', dest='linux_tgz_url',
+      default=None,
+      help='URL for the Linux tgz archive.')
+  parser.add_option(
+      '-n', '--bundle_name', dest='bundle_name',
+      default=None,
+      help='Required: Name of the bundle.')
+  parser.add_option(
+      '-r', '--recommended', dest='recommended',
+      default=None,
+      help='Required: True or False, whether this bundle is recommended.')
+  parser.add_option(
+      '-s', '--stability', dest='stability',
+      default=None,
+      help='Required: Stability for this bundle; one of. '
+           '"obsolete", "post_stable", "stable", "beta", "dev", "canary".')
+  parser.add_option(
+      '-u', '--desc_url', dest='bundle_desc_url',
+      default=None,
+      help='Optional: URL to follow to read additional bundle info.')
+  parser.add_option(
+      '-v', '--manifest_version', dest='manifest_version',
+      type='int',
+      default=None,
+      help='Required for new manifest files: '
+           'Version number for the manifest.')
+  parser.add_option(
+      '-W', '--win_tgz', dest='win_tgz_url',
+      default=None,
+      help='URL for the Windows tgz archive.')
 
-  # TODO(mball,gwink) Add logic to load and save manifest file.
+  # Parse options and arguments and check.
+  (options, args) = parser.parse_args(argv)
+  if len(args) > 1:
+    parser.error('Must provide at most one manifest file name')
+
+  filename = None
+  if len(args) == 1:
+    filename = str(args[0])
+  manifest_file = SDKManifestFile(filename)
+  manifest_file.UpdateWithOptions(options)
+
   return 0
 
 if __name__ == '__main__':
-  sys.exit(main())
+  sys.exit(main(sys.argv[1:]))
