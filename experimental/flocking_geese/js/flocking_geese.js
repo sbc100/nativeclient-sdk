4bd0492c87c9e4a47c4f9c497c348bb4e9b2530f
dspringer@google.com <dspringer@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-09-09 21:47:19 +0000

    Add a load progress bar.
    The load progress bar is fully Kennedy-compliant.
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1131 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- experimental/flocking_geese/js/flocking_geese.js
+++ experimental/flocking_geese/js/flocking_geese.js
@@ -98,8 +98,15 @@ FlockingGeese.DomIds = {
   // The <DIV> containing the simulation drawing area (either the NaCl module
   // or the <CANVAS>.
   NACL_VIEW: 'nacl_flocking_geese',
+  // The <DIV> containing all the load progress elements: title text, a
+  // progress bar and some status text.
+  PROGRESS: 'progress',
   // The <DIV> containing the load progress bar.
   PROGRESS_BAR: 'progress_bar',
+  // The element containing the progress text.
+  PROGRESS_BAR_TEXT: 'progress_bar_text',
+  // The progress bar track, the width of this element tracks progress.
+  PROGRESS_TRACK: 'progress_track',
   // The button used to select sim type (NaCL vs. JavaScript).  Contained in
   // the info panel.
   SIM_MODE_BUTTON: 'sim_mode_button',
@@ -196,10 +203,15 @@ FlockingGeese.prototype.moduleDidLoad = function(opt_naclModuleId) {
     this.invokeNaClMethod('resetFlock',
                           {'size' : parseInt(flockSizeSelect.value)});
   }
-  // Hide the load progress bar and make the modeul element visible.
-  var progress_bar = document.getElementById(FlockingGeese.DomIds.PROGRESS_BAR);
+  // Hide the load progress bar and make the module element visible.
+  var progress_bar = document.getElementById(FlockingGeese.DomIds.PROGRESS);
   progress_bar.style.visibility = 'hidden';
-  this.naclModule_.style.visibility = 'visible';
+  this.naclModule_.style.visibility = 'inherit';
+  // If the NaCL module is being displayed, then start the simulation.
+  var naclView = document.getElementById(FlockingGeese.DomIds.NACL_VIEW);
+  if (naclView.style.visibility == 'visible') {
+    this.invokeNaClMethod('runSimulation');
+  }
 }
 
 /**
@@ -323,7 +335,7 @@ FlockingGeese.prototype.handleNaClMessage = function(messageEvent) {
       if (parameter[0] == this.MethodSignatures_.FRAME_RATE) {
         var frameRate = parseFloat(parameter[1]);
         if (frameRate == 0.0) {
-          frameRate = this.speedomter_.maximumSpeed();
+          frameRate = this.speedometer_.maximumSpeed();
         }
         this.speedometer_.updateMeterNamed(FlockingGeese.MeterNames.NACL,
                                            frameRate);
@@ -334,6 +346,46 @@ FlockingGeese.prototype.handleNaClMessage = function(messageEvent) {
     var error = 'Unrecognized NaCl message: ' + messageEvent.data;
     console.log(error);
     throw new Error(error);
+  }
+}
+
+/**
+ * Handle a progress event by the NaCl module loader.  |progressEvent| contains
+ * a couple of interesting properties that are used in this example:
+ *     total The size of the NaCl module in bytes.  Note that this value
+ *         is 0 until |lengthComputable| is true.  In particular, this
+ *         value is 0 for the first 'progress' event.
+ *     loaded The number of bytes loaded so far.
+ *     lengthComputable A boolean indicating that the |total| field
+ *         represents a valid length.
+ * @param {Event} progressEvent The ProgressEvent that triggered this handler.
+ */
+FlockingGeese.prototype.handleProgressEvent = function(progressEvent) {
+  var loadPercent = 0.0;
+  var loadPercentString;
+  if (event.lengthComputable && event.total > 0) {
+    loadPercent = event.loaded / event.total;
+    loadPercentString = (loadPercent * 100.0).toFixed() + '%';
+  } else {
+    // The total length is not yet known.
+    loadPercent = -1.0;
+    loadPercentString = 'Computing size&hellip;';
+  }
+  var progressBarText = document.getElementById(
+      FlockingGeese.DomIds.PROGRESS_BAR_TEXT);
+  progressBarText.innerHTML = loadPercentString +
+    ' (' + event.loaded + ' of ' + event.total + ' bytes)';
+  var progressTrack = document.getElementById(
+      FlockingGeese.DomIds.PROGRESS_TRACK);
+  if (loadPercent >= 0.0) {
+    var progressBar =
+        document.getElementById(FlockingGeese.DomIds.PROGRESS_BAR);
+    var paddingBox = goog.style.getPaddingBox(progressBar);
+    var maxTrackWidth = progressBar.clientWidth -
+                        (paddingBox.left + paddingBox.right);
+    progressTrack.style.width = (loadPercent * maxTrackWidth).toFixed() + 'px';
+  } else {
+    progressTrack.style.width = '0px';
   }
 }
 
@@ -370,18 +422,24 @@ FlockingGeese.prototype.run = function(opt_viewDivName) {
   viewDiv.addEventListener('message',
                            goog.bind(this.handleNaClMessage, this),
                            true);
+  // Handle the load progress.
+  viewDiv.addEventListener('progress',
+                           goog.bind(this.handleProgressEvent, this),
+                           true);
 
   var viewSize = goog.style.getSize(viewDiv);
   viewDiv.innerHTML =
-    '<div id="' + FlockingGeese.DomIds.PROGRESS_BAR + '" ' +
-          'class="autosize-view" ' +
-          'style="width: ' + viewSize.width + '; ' +
-                '"height: ' + viewSize.height + ';">' +
-      '<p class="progressstatus">Loading NaCl Module&hellip;</p>' +
-      '<div class="progressbar tall">' +
-        '<div class="progresstrack"></div>' +
+    '<div id="' + FlockingGeese.DomIds.PROGRESS + '" ' + 'class="progress">' +
+      '<p class="progressstatus">' +
+          'Loading NaCl Module&hellip;</p>' +
+      '<div id="' + FlockingGeese.DomIds.PROGRESS_BAR + '" ' +
+          'class="progressbar tall">' +
+        '<div id="' + FlockingGeese.DomIds.PROGRESS_TRACK + '" ' +
+            'class="progresstrack"></div>' +
       '</div>' +
-      '<p class="progresstext">53% complete.</p>' +
+      '<p id="' + FlockingGeese.DomIds.PROGRESS_BAR_TEXT + '" ' +
+          'class="progresstext">' +
+          'Computing size&hellip;</p>' +
     '</div>' +
     '<embed id="' + FlockingGeese.DomIds.NACL_MODULE + '" ' +
           ' class="autosize-view"' +
@@ -389,8 +447,7 @@ FlockingGeese.prototype.run = function(opt_viewDivName) {
           ' height=' + viewSize.height +
           ' src="flocking_geese.nmf"' +
           ' type="application/x-nacl"' +
-          ' style="visibility:hidden;" />'
-
+          ' style="visibility: hidden;" />'
   this.startJavaScriptSimulation_();
 }
 
