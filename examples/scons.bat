4c14ebd2de80037e71ab8792606baa6a43548bee
dspringer@google.com <dspringer@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-09-26 19:40:13 +0000

    Fix windows build.
    BUG= http://code.google.com/p/nativeclient-sdk/issues/detail?id=135
    TEST=check out a fresh version of the SDK and try to build the examples with
    NO env vars set (NACL_SDK_ROOT and NACL_TARGET_PLATFORM in particular).
    Committed: http://code.google.com/p/nativeclient-sdk/source/detail?r=1208
    Review URL: http://codereview.chromium.org/8044023
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1209 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- examples/scons.bat
+++ examples/scons.bat
@@ -9,7 +9,10 @@ setlocal
 :: NACL_SDK_ROOT must be set.
 if not defined NACL_SDK_ROOT (
   echo Error: NACL_SDK_ROOT is not set.
-  exit 1
+  echo Please set NACL_SDK_ROOT to the full path of the Native Client SDK.
+  echo For example:
+  echo set NACL_SDK_ROOT=D:\nacl_sdk
+  goto end
 )
 
 :: NACL_TARGET_PLATFORM is really the name of a folder with the base dir -
@@ -19,7 +22,7 @@ if not defined NACL_SDK_ROOT (
 :: build applications that target the pepper_15 API, set
 ::   NACL_TARGET_PLATFORM=pepper_15
 if not defined NACL_TARGET_PLATFORM (
-  set NACL_TARGET_PLATFORM="pepper_15"
+  set NACL_TARGET_PLATFORM=pepper_15
 )
 
 set NACL_PLATFORM_DIR=%NACL_SDK_ROOT%\%NACL_TARGET_PLATFORM%
@@ -36,3 +39,5 @@ python -O -OO "%NACL_PLATFORM_DIR%\third_party\scons-2.0.1\script\scons" ^
 --warn no-visual-c-missing ^
 --file=build.scons ^
 --site-dir="%NACL_PLATFORM_DIR%\build_tools\nacl_sdk_scons" %*
+
+:end
