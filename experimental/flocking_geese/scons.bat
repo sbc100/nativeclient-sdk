b9b4ba0f0eee3265d146bf0272f4c3cc75040502
dspringer@google.com <dspringer@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-10-17 20:57:03 +0000

    Fix the build scripts so flocking_geese builds from anywhere.
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1271 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- experimental/flocking_geese/scons.bat
+++ experimental/flocking_geese/scons.bat
@@ -6,21 +6,34 @@
 
 setlocal
 
-set NACL_SDK_ROOT=%~dp0..\nacl-sdk\src
+:: NACL_SDK_ROOT must be set.
+if not defined NACL_SDK_ROOT (
+  echo Error: NACL_SDK_ROOT is not set.
+  echo Please set NACL_SDK_ROOT to the full path of the Native Client SDK.
+  echo For example:
+  echo set NACL_SDK_ROOT=D:\nacl_sdk
+  goto end
+)
+
 # NACL_TARGET_PLATFORM is really the name of a folder with the base dir -
 # usually nacl-sdk-root - within which the toolchain for the target platform
 # are found.
-set NACL_TARGET_PLATFORM=.
+set NACL_TARGET_PLATFORM=pepper_15
+
+set NACL_PLATFORM_DIR=%NACL_SDK_ROOT%\%NACL_TARGET_PLATFORM%
 
 :: Set the PYTHONPATH and SCONS_LIB_DIR so we can import SCons modules
-set SCONS_LIB_DIR=%~dp0third_party\scons-2.0.1\engine
-set PYTHONPATH=%NACL_SDK_ROOT%\third_party\scons-2.0.1\engine
+set SCONS_LIB_DIR=%NACL_PLATFORM_DIR%\third_party\scons-2.0.1\engine
+set PYTHONPATH=%NACL_PLATFORM_DIR%\third_party\scons-2.0.1\engine
 
 :: We have to do this because scons overrides PYTHONPATH and does not preserve
 :: what is provided by the OS.  The custom variable name won't be overwritten.
-set PYMOX=%NACL_SDK_ROOT%\third_party\pymox
+set PYMOX=%NACL_PLATFORM_DIR%\third_party\pymox
 
 :: Run the included copy of scons.
-python -O -OO "%NACL_SDK_ROOT%\third_party\scons-2.0.1\script\scons" ^
+python -O -OO "%NACL_PLATFORM_DIR%\third_party\scons-2.0.1\script\scons" ^
+--warn no-visual-c-missing ^
 --file=build.scons ^
---site-dir="%NACL_SDK_ROOT%\build_tools\nacl_sdk_scons" %*
+--site-dir="%NACL_PLATFORM_DIR%\build_tools\nacl_sdk_scons" %*
+
+:end
