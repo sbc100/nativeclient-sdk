a6249e69dcfc00ee94ee46eb03b1920a6f295860
mball@google.com <mball@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-10-19 21:13:55 +0000

    Updated create_nmf.py to merge both architectures into one nmf
    BUG= http://code.google.com/p/nativeclient-sdk/issues/detail?id=137
     http://code.google.com/p/nativeclient/issues/detail?id=2299
    TEST=run_nmf_test + bots
    
    Review URL: http://codereview.chromium.org/8343012
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1281 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- build_tools/nacl_sdk_scons/nmf_test.py
+++ build_tools/nacl_sdk_scons/nmf_test.py
@@ -19,9 +19,12 @@ from build_tools import build_utils
 from build_tools.nacl_sdk_scons.site_tools import create_nmf
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+X86_32 = 'x86-32'
+X86_64 = 'x86-64'
+ARCHS = [X86_32, X86_64]
 
 
-def CallGenerateManifest(args):
+def CallCreateNmf(args):
   '''Calls the create_nmf.py utility and returns stdout as a string
 
   Args:
@@ -32,13 +35,17 @@ def CallGenerateManifest(args):
 
   Raises:
     subprocess.CalledProcessError: non-zero return code from sdk_update'''
-  command = ['python', os.path.join(SCRIPT_DIR, 'site_tools',
-                                    'create_nmf.py')] + args
+  command = [sys.executable, os.path.join(SCRIPT_DIR, 'site_tools',
+                                          'create_nmf.py')] + args
+  print "\nCallCreateNmf(%s)" % command
+  sys.stdout.flush()
   process = subprocess.Popen(stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE,
                              args=command)
   output, error_output = process.communicate()
 
+  print "\noutput=%s\n" % output
+  sys.stdout.flush()
   retcode = process.poll() # Note - calling wait() can cause a deadlock
   if retcode != 0:
     print "stdout=%s\nstderr=%s" % (output, error_output)
@@ -46,8 +53,17 @@ def CallGenerateManifest(args):
   return output
 
 
-def GlobalInit(options):
+def GlobalInit(options, temp_files):
   '''Global initialization for testing nmf file creation.
+
+  Args:
+    options: object containing gcc and gpp defines
+    files: (output) A list that will contain all generated nexes.  Each
+        nexe is a value in a map, keyed by architecture.
+
+  Returns:
+    A list of filemaps, where each filemap is a dict with key=architecture
+        and value=filename
   '''
   hello_world_c = (
       '#include <stdio.h>\n'
@@ -56,41 +72,37 @@ def GlobalInit(options):
       '#include <iostream>\n'
       'int main(void) { std::cout << "Hello World!\\n"; return 0; }\n')
 
-  files = {32: [], 64: []}
-  def MakeNexe(contents, suffix, compiler, bits):
-    source_filename = None
-    try:
+  files = []
+  def MakeNexe(contents, suffix, compiler):
+    file_map = {}
+    for arch in ARCHS:
+      source_filename = None
       with tempfile.NamedTemporaryFile(delete=False,
                                        suffix=suffix) as temp_file:
         source_filename = temp_file.name
+        temp_files.append(source_filename)
         temp_file.write(contents)
       nexe_filename = os.path.splitext(source_filename)[0] + '.nexe'
-      files[bits].append(nexe_filename)
+      file_map[arch] = nexe_filename
+      temp_files.append(nexe_filename)
       subprocess.check_call([compiler,
-                             '-m32' if bits == 32 else '-m64',
+                             '-m32' if arch == X86_32 else '-m64',
                              source_filename,
                              '-o', nexe_filename])
-    finally:
-      if source_filename and os.path.exists(source_filename):
-        os.remove(source_filename)
+    files.append(file_map)
 
-  try:
-    for bits in build_utils.SupportedNexeBitWidths():
-      c_file = MakeNexe(hello_world_c, '.c', options.gcc, bits)
-      cc_file = MakeNexe(hello_world_cc, '.cc', options.gpp, bits)
-  except:
-    GlobalTeardown(files)
-    raise
-
+  c_file = MakeNexe(hello_world_c, '.c', options.gcc)
+  cc_file = MakeNexe(hello_world_cc, '.cc', options.gpp)
   return files
 
 
-def TestingClosure(toolchain_dir, bits, files):
+def TestingClosure(toolchain_dir, file_map):
   '''Closure to provide variables to the test cases
 
   Args:
     toolchain_dir: path to toolchain that we are testing
-    bits: bit width of nexe (either 32 or 64)'''
+    file_map: dict of nexe files by architecture.
+  '''
 
   class TestNmf(unittest.TestCase):
     ''' Test basic functionality of the sdk_update package '''
@@ -99,31 +111,29 @@ def TestingClosure(toolchain_dir, bits, files):
       self.objdump = (os.path.join(toolchain_dir,
                                    'bin',
                                    'x86_64-nacl-objdump'))
-      self.library_path = os.path.join(toolchain_dir,
-                                       'x86_64-nacl',
-                                       'lib32' if bits == 32 else 'lib')
+      self.library_paths = [os.path.join(toolchain_dir, 'x86_64-nacl', lib)
+                            for lib in ['lib32', 'lib']]
+      self.lib_options = ['--library-path=%s' % lib
+                          for lib in self.library_paths]
 
-    def testRunGenerateManifest(self):
-      for file in files:
-        json_text = CallGenerateManifest(
-            ['--library-path', self.library_path,
-             '--objdump', self.objdump,
-             file])
-        obj = json.loads(json_text)
-        # For now, just do a simple sanity check that there is a file
-        # and program section.
-        # TODO(mball) Add more tests
-        self.assertTrue(obj.get('files'))
-        self.assertTrue(obj.get('program'))
+    def testRunCreateNmf(self):
+      json_text = CallCreateNmf(
+          self.lib_options + ['--objdump', self.objdump] + file_map.values())
+      obj = json.loads(json_text)
+      # For now, just do a simple sanity check that there is a file
+      # and program section.
+      # TODO(mball) Add more tests
+      self.assertTrue(obj.get('files'))
+      self.assertTrue(obj.get('program'))
 
   return TestNmf
 
 
 def GlobalTeardown(temp_files):
-  for filelist in temp_files.values():
-    for filename in filelist:
-      if os.path.exists(filename):
-        os.remove(filename)
+  '''Remove all the temporary files in temp_files'''
+  for filename in temp_files:
+    if os.path.exists(filename):
+      os.remove(filename)
 
 
 def main(argv):
@@ -144,16 +154,13 @@ def main(argv):
                                  'x86_64-nacl-objdump')
 
   success = True
-  temp_files = {32: [], 64: []}
+  temp_files = []
   try:
-    temp_files = GlobalInit(options)
-    for bits in [32, 64]:
-      nexe_files = [name for name in temp_files[bits]
-                    if os.path.splitext(name)[1] == '.nexe']
+    nexe_maps = GlobalInit(options, temp_files)
+    for file_map in nexe_maps:
       suite = unittest.TestLoader().loadTestsFromTestCase(
           TestingClosure(toolchain_dir=options.toolchain_dir,
-                         bits=bits,
-                         files=nexe_files))
+                         file_map=file_map))
       result = unittest.TextTestRunner(verbosity=2).run(suite)
       success = result.wasSuccessful() and success
   finally:
--- build_tools/nacl_sdk_scons/site_tools/create_nmf.py
+++ build_tools/nacl_sdk_scons/site_tools/create_nmf.py
@@ -15,16 +15,17 @@ except ImportError:
   import simplejson as json
 
 NeededMatcher = re.compile('^ *NEEDED *([^ ]+)\n$')
-FormatMatcher = re.compile('^.*file format (.+)\n$')
+FormatMatcher = re.compile('^(.+):\\s*file format (.+)\n$')
 
 FormatArchMap = {
     # Names returned by Linux's objdump:
     'elf64-x86-64': 'x86-64',
     'elf32-i386': 'x86-32',
-    'elf32-little': 'arm',
     # Names returned by x86_64-nacl-objdump:
     'elf64-nacl': 'x86-64',
     'elf32-nacl': 'x86-32',
+    # TODO(mball): Add support for 'arm-32' and 'portable' architectures
+    # 'elf32-little': 'arm-32',
     }
 
 
@@ -33,82 +34,205 @@ class Error(Exception):
   pass
 
 
-def GleanFromObjdump(files, objdump='objdump'):
-  proc = subprocess.Popen([objdump, '-p'] + files,
-                          stdout=subprocess.PIPE, bufsize=-1)
-  format = None
-  needed = set()
-  output, _ = proc.communicate()
-  for line in output.splitlines(True):
-    matched = NeededMatcher.match(line)
-    if matched is not None:
-      needed.add(matched.group(1))
-    elif format is None:
+class ArchFile(object):
+  '''Simple structure containing information about
+
+  Attributes:
+    arch: Architecture of this file (e.g., x86-32)
+    filename: name of this file
+    path: Full path to this file on the build system
+    url: Relative path to file in the staged web directory.
+        Used for specifying the "url" attribute in the nmf file.'''
+  def __init__(self, arch, name, path='', url=None):
+    self.arch = arch
+    self.name = name
+    self.path = path
+    self.url = url or '/'.join([arch, name])
+
+  def __str__(self):
+    '''Return the file path when invoked with the str() function'''
+    return self.path
+
+
+class NmfUtils(object):
+  '''Helper class for creating and managing nmf files'''
+
+  def __init__(self, main_files=None, objdump='x86_64-nacl-objdump',
+               lib_path=None, extra_files=None):
+    ''' Constructor
+
+    Args:
+      main_files: List of main entry program files.  These will be named
+          files->main.nexe for dynamic nexes, and program for static nexes
+      objdump: path to x86_64-nacl-objdump tool (or Linux equivalent)'''
+    self.objdump = objdump
+    self.main_files = main_files or []
+    self.extra_files = extra_files or []
+    self.lib_path = lib_path or []
+    self.manifest = None
+
+  def GleanFromObjdump(self, files):
+    '''Get architecture and dependency information for given files
+
+    Args:
+      files: A dict with key=filename and value=list or set of archs.  E.g.:
+          { '/path/to/my.nexe': ['x86-32', 'x86-64'],
+            '/path/to/libmy.so': ['x86-32'],
+            '/path/to/my2.nexe': None }  # Indicates all architectures
+
+    Returns: A tuple with the following members:
+      input_info: A dict with key=filename and value=ArchFile of input files.
+          Includes the input files as well, with arch filled in if absent.
+          Example: { '/path/to/my.nexe': ArchFile(my.nexe),
+                     '/path/to/libfoo.so': ArchFile(libfoo.so) }
+      needed: A set of strings formatted as "arch/name".  Example:
+          set(['x86-32/libc.so', 'x86-64/libgcc.so'])
+    '''
+    proc = subprocess.Popen([self.objdump, '-p'] + files.keys(),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, bufsize=-1)
+    input_info = {}
+    needed = set()
+    output, err_output = proc.communicate()
+    for line in output.splitlines(True):
+      # Objdump should display the architecture first and then the dependencies
+      # second for each file in the list.
       matched = FormatMatcher.match(line)
       if matched is not None:
-        format = matched.group(1)
-  status = proc.poll()
-  if status != 0:
-    raise RuntimeError('objdump failed: %d' % status)
-  try:
-    arch = FormatArchMap[format]
-  except KeyError:
-    raise RuntimeError('objdump found unrecognized format: %s' % format)
-  return arch, needed
+        filename = matched.group(1)
+        arch = FormatArchMap[matched.group(2)]
+        if files[filename] is None or arch in files[filename]:
+          input_info[filename] = ArchFile(arch=arch,
+                                          name=os.path.basename(filename),
+                                          path=filename)
+      matched = NeededMatcher.match(line)
+      if matched is not None:
+        if files[filename] is None or arch in files[filename]:
+          needed.add('/'.join([arch, matched.group(1)]))
+    status = proc.poll()
+    if status != 0:
+      raise Error('%s\nStdError=%s\nobjdump failed with error code: %d' %
+                  (output, err_output, status))
+    return input_info, needed
 
+  def FindLibsInPath(self, name):
+    '''Finds the set of libraries matching |name| within lib_path
 
-def FindLibInPath(name, path):
-  for dir in path:
-    file = os.path.join(dir, name)
-    if os.path.exists(file):
-      return file
-  raise RuntimeError('cannot find library %s' % name)
+    Args:
+      name: name of library to find
 
+    Returns:
+      A list of system paths that match the given name within the lib_path'''
+    files = []
+    for dir in self.lib_path:
+      file = os.path.join(dir, name)
+      if os.path.exists(file):
+        files.append(file)
+    if not files:
+      raise Error('cannot find library %s' % name)
+    return files
 
-def CollectNeeded(file, lib_path, objdump='objdump'):
-  examined = set()
-  files_to_examine = [file]
-  arch = None
-  while files_to_examine:
-    foundarch, needed = GleanFromObjdump(files_to_examine, objdump)
-    if arch is None:
-      arch = foundarch
-    elif foundarch != arch:
-      raise RuntimeError('unexpected mixing of %s and %s files' %
-                         (arch, foundarch))
-    unexamined = needed - examined
-    files_to_examine = [FindLibInPath(name, lib_path) for name in unexamined]
-    examined |= unexamined
-  return arch, examined
+  def CollectNeeded(self, files):
+    '''Collect the list of dependencies for a given set of files
 
+    Args:
+      files: A list of filenames to examine
 
-def ListManifestPrograms(filename, select_arch=None):
-  file = open(filename, 'r')
-  tree = json.load(file)
-  file.close()
-  program_tree = tree['program']
-  if select_arch is None:
-    return [program_tree[arch]['url'] for arch in program_tree]
-  else:
-    return [program_tree[select_arch]['url']]
+    Returns:
+      A dict with key=filename and value=ArchFile of input files.
+          Includes the input files as well, with arch filled in if absent.
+          Example: { '/path/to/my.nexe': ArchFile(my.nexe),
+                     '/path/to/libfoo.so': ArchFile(libfoo.so) }'''
+    examined = set()
+    all_files, needed = self.GleanFromObjdump(
+        dict([(file, None) for file in files]))
+    unexamined = needed
+    while unexamined:
+      files_to_examine = {}
+      for arch_name in unexamined:
+        arch, name = arch_name.split('/')
+        for path in self.FindLibsInPath(name):
+          files_to_examine.setdefault(path, set()).add(arch)
+      new_files, needed = self.GleanFromObjdump(files_to_examine)
+      all_files.update(new_files)
+      examined |= unexamined
+      unexamined = needed - examined
+    return all_files
 
+  def ListManifestPrograms(self, filename, select_arch=None):
+    '''Note: This function is currently used and untested'''
+    file = open(filename, 'r')
+    tree = json.load(file)
+    file.close()
+    program_tree = tree['program']
+    if select_arch is None:
+      return [program_tree[arch]['url'] for arch in program_tree]
+    else:
+      return [program_tree[select_arch]['url']]
 
-def WriteJson(tree, output):
-  json.dump(tree, output, indent=2)
-  output.write('\n')
+  def WriteJson(self, tree, output):
+    json.dump(tree, output, indent=2)
+    output.write('\n')
+
+  def _GenerateManifest(self):
+    programs = {}
+    files = {}
+
+    def add_files(needed):
+      for filename, arch_file in needed.items():
+        files.setdefault(arch_file.arch, set()).add(arch_file.name)
+
+    needed = self.CollectNeeded(self.main_files)
+    add_files(needed)
+
+    for filename in self.main_files:
+      arch_file = needed[filename]
+      programs[arch_file.arch] = arch_file.name
+
+    # With the runnable-ld.so scheme we have today, the proper name of
+    # the dynamic linker should be excluded from the list of files.
+    for arch in files:
+      ldso = 'ld-nacl-%s.so.1' % arch
+      try:
+        files[arch].remove(ldso)
+      except KeyError:
+        pass
+
+    filemap = {}
+    for arch in files:
+      for file in files[arch]:
+        if file not in programs.values():
+          filemap.setdefault(file, set()).add(arch)
+
+    def arch_name(arch, file):
+      # nmf files expect unix-style path separators
+      return {'url': '/'.join([arch, file])}
+
+    # TODO(mcgrathr): perhaps notice a program with no deps
+    # (i.e. statically linked) and generate program=nexe instead?
+    manifest = {'program': {}, 'files': {'main.nexe': {}}}
+    for arch in programs:
+      manifest['program'][arch] = arch_name(arch, 'runnable-ld.so')
+      manifest['files']['main.nexe'][arch] = arch_name(arch, programs[arch])
+
+    for file in filemap:
+      manifest['files'][file] = dict([(arch, arch_name(arch, file))
+                                      for arch in filemap[file]])
+    self.manifest = manifest
+
+  def GetManifest(self):
+    '''Returns a JSON-formatted dict containing the NaCl dependencies'''
+    if not self.manifest:
+      self._GenerateManifest()
+
+    return self.manifest
 
 
 def Main(argv):
   parser = optparse.OptionParser(
       usage='Usage: %prog [options] nexe [extra_libs...]')
-  parser.add_option('-m', '--nmf', dest='nmf',
-                    help='Read program file name from existing manifest FILE',
-                    metavar='FILE')
-  parser.add_option('-A', '--arch', dest='arch',
-                    help='Include only files built for ARCH',
-                    metavar='ARCH')
   parser.add_option('-o', '--output', dest='output',
-                    help='Write manifest file to FILE',
+                    help='Write manifest file to FILE (default is stdout)',
                     metavar='FILE')
   parser.add_option('-D', '--objdump', dest='objdump', default='objdump',
                     help='Use TOOL as the "objdump" tool to run',
@@ -117,82 +241,23 @@ def Main(argv):
                     action='append', default=[],
                     help='Add DIRECTORY to library search path',
                     metavar='DIRECTORY')
-  parser.add_option('-N', '--no-arch-prefix', dest='add_arch_prefix',
-                    action='store_false', default=True,
-                    help='Omit architecture subdirectory prefix from URLs')
   (options, args) = parser.parse_args(argv)
 
-  if options.nmf is not None:
-    nexes = ListManifestPrograms(options.nmf, select_arch=options.arch)
-    extras = args
-  elif len(args) < 1:
+  if len(args) < 1:
     parser.print_usage()
     sys.exit(1)
-  else:
-    nexes = args[0:1]
-    extras = args[1:]
 
-  programs = {}
-  files = {}
+  nmf = NmfUtils(objdump=options.objdump,
+                 main_files=args,
+                 lib_path=options.lib_path)
 
-  def add_files(arch, needed):
-    if arch not in files:
-      files[arch] = set()
-    files[arch] |= needed
-
-  for nexe in nexes:
-    if options.nmf is not None and not os.path.exists(nexe):
-      nexe = FindLibInPath(os.path.basename(nexe), options.lib_path)
-    arch, needed = CollectNeeded(nexe, options.lib_path, options.objdump)
-    if options.arch is None or arch == options.arch:
-      if arch in programs:
-        raise RuntimeError('multiple programs for architecture %s' % arch)
-      programs[arch] = os.path.basename(nexe)
-      add_files(arch, needed)
-
-  for extra in extras:
-    arch, needed = CollectNeeded(extra, options.lib_path, options.objdump)
-    if options.arch is None or arch == options.arch:
-      add_files(arch, needed)
-
-  # With the runnable-ld.so scheme we have today, the proper name of
-  # the dynamic linker should be excluded from the list of files.
-  for arch in files:
-    ldso = 'ld-nacl-%s.so.1' % arch
-    try:
-      files[arch].remove(ldso)
-    except KeyError:
-      pass
-
-  filemap = {}
-  for arch in files:
-    for file in files[arch]:
-      if file not in filemap:
-        filemap[file] = set()
-      filemap[file].add(arch)
-
-  def arch_name(arch, file):
-    if options.add_arch_prefix:
-      return os.path.join(arch, file)
-    else:
-      return file
-
-  # TODO(mcgrathr): perhaps notice a program with no deps
-  # (i.e. statically linked) and generate program=nexe instead?
-  manifest = {'program': {}, 'files': {'main.nexe': {}}}
-  for arch in programs:
-    manifest['program'][arch] = arch_name(arch, 'runnable-ld.so')
-    manifest['files']['main.nexe'][arch] = arch_name(arch, programs[arch])
-
-  for file in filemap:
-    manifest['files'][file] = dict([(arch, {'url': arch_name(arch, file)})
-                                    for arch in filemap[file]])
+  manifest = nmf.GetManifest()
 
   if options.output is None:
-    WriteJson(manifest, sys.stdout)
+    nmf.WriteJson(manifest, sys.stdout)
   else:
     output = open(options.output, 'w')
-    WriteJson(manifest, output)
+    nmf.WriteJson(manifest, output)
     output.close()
 
 
