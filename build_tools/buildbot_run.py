46167e5d5d633a233ffca79a330730383d56028f
bradnelson@google.com <bradnelson@google.com@050acbb0-2703-11df-ab0a-9f3f633ae91d>
2011-11-28 21:08:25 +0000

    Make archive source independent of where buildbot_run.py is run from.
    BUG=None
    TEST=None
    R=noelallen@google.com
    Review URL: http://codereview.chromium.org/8726013
    
    git-svn-id: https://nativeclient-sdk.googlecode.com/svn/trunk/src@1391 050acbb0-2703-11df-ab0a-9f3f633ae91d

--- build_tools/buildbot_run.py
+++ build_tools/buildbot_run.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python2.6
-#
 # Copyright (c) 2011 The Native Client Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -11,8 +10,10 @@ import subprocess
 import sys
 
 # Add scons to the python path (as nacl_utils.py requires it).
-PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(PARENT_DIR, 'third_party/scons-2.0.1/engine'))
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+SDK_SRC_DIR = os.path.dirname(SCRIPT_DIR)
+SDK_DIR = os.path.dirname(SDK_SRC_DIR)
+sys.path.append(os.path.join(SDK_SRC_DIR, 'third_party/scons-2.0.1/engine'))
 
 import build_utils
 
@@ -35,10 +36,11 @@ def Archive(revision, chrome_milestone):
     dst = 'naclsdk_linux.tgz'
   bucket_path = 'nativeclient-mirror/nacl/nacl_sdk/pepper_%s_%s/%s' % (
       chrome_milestone, revision, dst)
+  full_src = os.path.join(SDK_DIR, src)
   full_dst = 'gs://%s' % bucket_path
   subprocess.check_call(
       '/b/build/scripts/slave/gsutil cp -a public-read %s %s' % (
-          src, full_dst), shell=True)
+          full_src, full_dst), shell=True)
   url = 'https://commondatastorage.googleapis.com/%s' % bucket_path
   print '@@@STEP_LINK@download@%s@@@' % url
   sys.stdout.flush()
